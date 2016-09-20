b01fdfe37524be657ff7bdee71c90a0c1a7527a4
olyaoya <olyaoya@gmail.com>
2016-09-20 20:10:44 +0300

    test

--- src/tests/first_test.js
+++ src/tests/first_test.js
@@ -1,9 +1,6 @@
 module.exports = {
     /*First test*/
-    "Search Justin Biber songs": function (browser) {
-        browser
-            .url("https://www.google.com.ua")
-            .setValue("[id=\"lst-ib\"]", "Justin Biber songs")
-            .click(".lsb[type=\"submit\"]")
+    "Search Justin Biber song": function (browser) {
+        browser.url("https://www.google.com.ua");
     }
 };
