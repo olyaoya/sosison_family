033f6afeeee0f6794fa75c3f2851acd63337dc18
olyaoya <olyaoya@gmail.com>
2016-09-20 20:22:59 +0300

    add comments

--- src/tests/first_test.js
+++ src/tests/first_test.js
@@ -2,8 +2,11 @@ module.exports = {
     /*First test*/
     "Search Justin Biber songs": function (browser) {
         browser
+            //access google site
             .url("https://www.google.com.ua")
+            //enter "Justin Biber songs" in search field
             .setValue("[id=\"lst-ib\"]", "Justin Biber songs")
+            //click search button
             .click(".lsb[type=\"submit\"]")
     }
 };
